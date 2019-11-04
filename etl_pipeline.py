4f0ca9d77abc6d54806760679ba2a3d5959ea6da
--replace-all <manishraj013@gmail.com>
2019-11-04 18:36:36 +0530

    changes to save_data fn

--- etl_pipeline.py
+++ etl_pipeline.py
@@ -34,7 +34,7 @@ def clean_data(df):
     df = df[~df.duplicated()]
 
 def save_data(df, database_filename):
-    engine = create_engine('sqlite:///TwitterDisasterDB.db')
+    engine = create_engine('sqlite:///' + database_filename)
     df.to_sql('twt_disaster_clean', engine, if_exists='replace',index=False)
 
 
